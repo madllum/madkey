dea1d22c8d03a851a53c42c5721cc91fe244e567
79658 <kvarsegova@list.ru>
2024-09-27 00:56:52 +0400

    dfgfdg

---
+++ index.py
@@ -0,0 +1,16 @@
+class MyClass:
+  """
+  Описание класса MyClass
+  """
+
+  def __init__(self, name):
+    """
+    Инициализация класса
+    """
+    self.name = name
+
+  def say_hello(self):
+    """
+    Метод для приветствия
+    """
+    print(f"Привет, я {self.name}!")
